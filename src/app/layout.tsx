ff1e16ece7f290cf6cafc1a73436c13697ba1ad2
Lance Mungcal <lance@example.com>
2025-07-17 22:31:11 +0800

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Dream Team Activity",
+  description: "Activity",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <main className="flex w-full bg-white">
+        <h1>Hello World</h1>
+      </main>
+      <body>{children}</body>
+    </html>
+  );
+}
